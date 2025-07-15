33ae9942ff94d77d4863e0cadc525a4ce4e898a1
blockenters <58394003+blockenters@users.noreply.github.com>
2025-07-15 10:37:57 +0900

    Create config.py

---
+++ project/config.py
@@ -0,0 +1,18 @@
+
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    db_host: str
+    db_port: int
+    db_name: str
+    db_user: str
+    db_password: str
+
+    @property
+    def database_url(self):
+        return f"mysql+pymysql://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
